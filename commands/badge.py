a24753992fc4a09c6694b87df6802db15cf4b051
Matt W <foggyio@outlook.com>
2020-08-28 15:04:31 +0100

    Sanitize SQL Input

--- commands/badge.py
+++ commands/badge.py
@@ -19,14 +19,14 @@ class Badge(commands.Cog):
             if operation.upper() == "ADD":
                 current_badges = get_profile(str(user.id))[3]
                 new_badges = current_badges + badge +" _ _"
-                sql.execute_query("UPDATE Members SET Badges = '%s' WHERE UserID = %s" % (new_badges, str(user.id)))
+                sql.execute_query("UPDATE Members SET Badges = '%s' WHERE UserID = %s" % (new_badges.replace("'", "''"), str(user.id)))
                 await message.channel.send(":ok_hand: Successfully added %s to **%s**'s profile!" % (badge, user.name))
 
             if operation.upper() == "REMOVE":
                 badge = badge + " _ _"
                 current_badges = get_profile(str(user.id))[3]
                 new_badges = current_badges.replace(badge, '')
-                sql.execute_query("UPDATE Members SET Badges = '%s' WHERE UserID = %s" % (new_badges, str(user.id)))
+                sql.execute_query("UPDATE Members SET Badges = '%s' WHERE UserID = %s" % (new_badges.replace("'", "''"), str(user.id)))
                 await message.channel.send(":ok_hand: Successfully removed %s from **%s**'s profile!" % (badge, user.name))
         else:
             await ctx.send("**Insufficient Permissions:** This command requires permission rank `MANAGER`")
