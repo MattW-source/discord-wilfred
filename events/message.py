ebc145f85bc68305e201222ac67c0a78fbb65a8d
Matt W <foggyio@outlook.com>
2020-10-02 22:14:27 +0100

    Team Points

--- events/message.py
+++ events/message.py
@@ -117,11 +117,11 @@ class On_Message(commands.Cog):
                     newWeeklyPoints = currentWeeklyPoints + 1
                     sql.execute_query("UPDATE Members set weeklyActivity = %s WHERE UserID = %s" % (str(newWeeklyPoints), str(message.author.id)))
                 if "Red Team" in [role.name for role in message.author.roles]:
-                    currentTeamPoints = sql.db_query("SELECT teamPoints FROM teams WHERE TeamName='RED'")[0][0]
+                    currentTeamPoints = sql.db_query("SELECT teamPoints FROM teams WHERE teamName='RED'")[0][0]
                     newTeamPoints = currentTeamPoints + 1
                     sql.execute_query("UPDATE teams SET teamPoints = %s WHERE TeamName='RED'" % (str(newTeamPoints)))
                 elif "Blue Team" in [role.name for role in message.author.roles]:
-                    currentTeamPoints = sql.db_query("SELECT teamPoints FROM teams WHERE TeamName='BLUE'")[0][0]
+                    currentTeamPoints = sql.db_query("SELECT teamPoints FROM teams WHERE teamName='BLUE'")[0][0]
                     newTeamPoints = currentTeamPoints + 1
                     sql.execute_query("UPDATE teams SET teamPoints = %s WHERE TeamName='BLUE'" % (str(newTeamPoints)))
                 level = get_profile(message.author.id)[1]
