67281045bc921c90019178b69b08fe886ec33ef9
Matt W <foggyio@outlook.com>
2020-11-25 20:43:45 +0000

    Update values-redacted.py

--- utils/values-redacted.py
+++ utils/values-redacted.py
@@ -6,3 +6,16 @@ token        = ""
 db_host     = ""
 db_user     = ""
 db_password = ""
+
+# Channel IDs
+general     = 547122996717027328
+commands    = 547123009694203905
+
+# Message IDs
+rules         = None
+rules2        = None # May not be needed if we condense down the rules and link to the doc  
+comps         = None
+lvl_rewards   = None
+badges        = None
+server_invite = None
+misc          = None  
