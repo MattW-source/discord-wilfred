da381ead754052da0e8402e85c64cfb3125caa48
Matt W <foggyio@outlook.com>
2020-11-27 20:46:24 +0000

    round 1 fixes

--- commands/setmessage.py
+++ commands/setmessage.py
@@ -21,17 +21,17 @@ class SetMessage(commands.Cog):
         '''
         if "Manager" in [role.name for role in ctx.message.author.roles]:
             if message_name.lower() in ["rules", "rules2", "comps", "lvl_rewards", "badges", "server_invite", "misc"]: # Limit what values will be accepted
-                message_id    = eval("value." + message_name.lower())
-                message_obj   = await self.client.fetch_message(message_id)
-                message_embed = discord.Embed(description=message_contents, color=colour.primary)
+                message_id      = eval("value." + message_name.lower())
+                message_channel = self.client.get_channel(781258948783112264)
+                message_obj     = await message_channel.fetch_message(message_id)
+                message_embed   = discord.Embed(description=message_contents, color=colour.primary)
                 message_embed.set_author(name=message_title)
-                await message_obj.edit(content=None, embed=embed)
+                await message_obj.edit(content=None, embed=message_embed)
                 await ctx.send("Successfully edited message!")
             else:
                 await ctx.send("Invalid Message Name")
         else:
             await ctx.send("You have insufficient privilidges to do this")
-
 
 def setup(client):
     client.add_cog(SetMessage(client))
