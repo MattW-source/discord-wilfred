81771b440286eb7ab5a06c630d1ec8eff5e75951
Matt W <foggyio@outlook.com>
2021-01-30 16:47:48 +0000

    Code Cleanup

--- commands/badge.py
+++ commands/badge.py
@@ -14,22 +14,21 @@ class Badge(commands.Cog):
 
     @commands.command()
     async def badge(self, ctx, operation, badge, user : discord.Member):
-        message = ctx.message
-        if "Manager" in [role.name for role in message.author.roles]:
+        if "Manager" in [role.name for role in ctx.message.author.roles]:
             if operation.upper() == "ADD":
-                current_badges = get_profile(str(user.id))[3]
-                new_badges = current_badges + badge +" _ _"
-                sql.execute_query("UPDATE Members SET Badges = '%s' WHERE UserID = %s" % (new_badges.replace("'", "''"), str(user.id)))
-                await message.channel.send(":ok_hand: Successfully added %s to **%s**'s profile!" % (badge, user.name))
+                current_badges = get_profile(str(user.id))[3] # Get Current Badges From Profile
+                new_badges = current_badges + badge +" _ _" # Format Badges String and append new badge
+                sql.execute_query("UPDATE Members SET Badges = '%s' WHERE UserID = %s" % (new_badges.replace("'", "''"), str(user.id))) # Store it
+                await ctx.send(":ok_hand: Successfully added %s to **%s**'s profile!" % (badge, user.name)) # Tell the user the operation is complete
 
             if operation.upper() == "REMOVE":
-                badge = badge + " _ _"
-                current_badges = get_profile(str(user.id))[3]
-                new_badges = current_badges.replace(badge, '')
-                sql.execute_query("UPDATE Members SET Badges = '%s' WHERE UserID = %s" % (new_badges.replace("'", "''"), str(user.id)))
-                await message.channel.send(":ok_hand: Successfully removed %s from **%s**'s profile!" % (badge, user.name))
+                badge = badge + " _ _" # Format Badge String
+                current_badges = get_profile(str(user.id))[3] # Get Current Badges From Profile
+                new_badges = current_badges.replace(badge, '') # Format badges string with selected badge removed
+                sql.execute_query("UPDATE Members SET Badges = '%s' WHERE UserID = %s" % (new_badges.replace("'", "''"), str(user.id))) # Store it
+                await ctx.send(":ok_hand: Successfully removed %s from **%s**'s profile!" % (badge, user.name)) # Tell the user the operation is complete
         else:
-            await ctx.send("**Insufficient Permissions:** This command requires permission rank `MANAGER`")
+            await ctx.send("**Insufficient Permissions:** This command requires permission rank `MANAGER`") # Insufficient Permissions
 
 def setup(client):
-    client.add_cog(Badge(client))
+    client.add_cog(Badge(client)) # Add cog/command
