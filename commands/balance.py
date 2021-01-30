102f9fb9f7ebb7616153dbbebca87a05b2a7df9f
Matt W <foggyio@outlook.com>
2021-01-30 16:49:52 +0000

    Code Cleanup

--- commands/balance.py
+++ commands/balance.py
@@ -13,18 +13,16 @@ class Balance(commands.Cog):
 
     @commands.command(aliases=["bal", "money"])
     async def balance(self, ctx, target : discord.Member = None):
-        if target == None:
-            balance = get_profile(ctx.author.id)[0]
-            target = ctx.author
-        else:
-            balance = get_profile(target.id)[0]
-        embed = discord.Embed(description="%s currently has **$%s**!" % (target.mention, str(balance)), color=colour.primary)
-        embed.set_author(name="Balance")
-        await ctx.send(embed=embed)
+        if target == None: # If this parameter is not provided, default to the person invoking the command
+            target = ctx.author # Set target to command invoker
+        balance = get_profile(target.id)[0] # Get balance from profile
+        embed = discord.Embed(description="%s currently has **$%s**!" % (target.mention, str(balance)), color=colour.primary) # Display Balance
+        embed.set_author(name="Balance") # Set embed author (to be used as title)
+        await ctx.send(embed=embed) # Send the embed
 
     @balance.error
     async def on_balance_error(self, ctx, error):
-        await ctx.send("Something went wrong, did you provide a valid user?")
+        await ctx.send("Something went wrong, did you provide a valid user?") # Most likely error is that the user doesn't exist
 
 def setup(client):
-    client.add_cog(Balance(client))
+    client.add_cog(Balance(client)) # Add cog/command
