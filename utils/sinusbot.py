76a40171716f83ca0a6006072b8a663ffdd91a58
Matt W <foggyio@outlook.com>
2022-02-19 19:20:02 +0000

    Set timeout on request

--- utils/sinusbot.py
+++ utils/sinusbot.py
@@ -12,7 +12,7 @@ def time_phaser(seconds):
     return output
 
 def login():
-    r = requests.post("http://music.foggyio.uk:8087/api/v1/bot/login", data={"username":"automation", "password":"", "botId":"d3170c67-312a-4093-98bb-e213696d58a4"})
+    r = requests.post("http://music.foggyio.uk:8087/api/v1/bot/login", data={"username":"automation", "password":"", "botId":"d3170c67-312a-4093-98bb-e213696d58a4"}, timeout=10.0)
     token = json.loads(r.text)["token"]
     return token
 
@@ -20,7 +20,7 @@ def playing():
     try:
         authToken = login()
         headers = {"authorization":"Bearer " + authToken}
-        r = requests.get("http://music.foggyio.uk:8087/api/v1/bot/i/a2a47bec-f1a3-4e91-85e2-eea4fa982b05/status", headers=headers)
+        r = requests.get("http://music.foggyio.uk:8087/api/v1/bot/i/a2a47bec-f1a3-4e91-85e2-eea4fa982b05/status", headers=headers, timeout=10.0)
         version = json.loads(r.text)["v"]
         try:
             song = json.loads(r.text)["currentTrack"]["tempArtist"] + " - " + json.loads(r.text)["currentTrack"]["tempTitle"]
